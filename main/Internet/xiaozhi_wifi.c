0c57d3851e74c92873cc1a629b8e1c62598ab47b
带特朗普去吃刘文祥 <lin199823@outlook.com>
2026-05-23 23:55:26 +0800

    移除 Wi-Fi 初始化标志和重试变量的定义

--- main/Internet/xiaozhi_wifi.c
+++ main/Internet/xiaozhi_wifi.c
@@ -16,9 +16,7 @@ static EventGroupHandle_t s_wifi_event_group;
 
 void xiaozhi_wifi_init(void)
 {
-    //!提交测试
-    uint8_t wifi_init_flag = 0;
-    uint8_t wifi_init_retry = 0;
+
     // 存储器初始化
     xiaozhi_wifi_nvs_Init();
     // 创建事件标志组
